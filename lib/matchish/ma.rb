c26898bd1929b86b5b3d7fafbad302e1a92a62a0
zverok <zverok.offline@gmail.com>
2015-07-18 23:48:13 +0300

    Even more simplification

--- lib/matchish/ma.rb
+++ lib/matchish/ma.rb
@@ -1,17 +1,5 @@
-Ma = Matchish
-
 class Object
-  alias_method :ma, :matchish
-end
-
-class Array
-  alias_method :ma, :matchish
-end
-
-class Hash
-  alias_method :ma, :matchish
-end
-
-class Class
-  alias_method :ma, :matchish
+  def ma(*arg)
+    matchish(*arg)
+  end
 end
